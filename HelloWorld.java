4741175e95da3ca4a7651e3f99b5f3736ddea03c
Josue <josuebasilis@gmail.com>
2021-06-23 14:44:47 -0400

    Hello World program

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+    //THis is a comment
+    /* Between the {} after the class declaration defines the body of 
+    class
+    */
+
+    //THE MAIN METHOD
+    //The start of the program
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
